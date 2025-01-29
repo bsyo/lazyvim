a5b3691678593ea3138700ddcae0dda2bb8c0449
bsyo <119839369+bsyo@users.noreply.github.com>
2025-01-29 23:06:01 +0900

    chore: update plugin versions in lazy-lock.json and lazyvim.json
    - Updated commit hashes for several plugins in lazy-lock.json
    - Removed leap plugin from lazyvim.json
    - Added new lazy loading options for avante.nvim and other plugins

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,55 +1,91 @@
 return {
   {
     "yetone/avante.nvim",
+    lazy = true,
     event = "VeryLazy",
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+
     opts = {
       provider = "copilot",
       auto_suggestions_provider = "copilot",
       copilot = {
         model = "claude-3.5-sonnet",
+        temperature = 0,
+        max_tokens = 8192,
       },
-      -- add any opts here
+      hints = { enabled = false },
+      file_selector = {
+        provider = "snacks",
+        provider_opts = {},
+      },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+
     dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = function(_, ft)
+          vim.list_extend(ft, { "Avante" })
+        end,
+      },
+      {
+        "folke/which-key.nvim",
         opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
+          spec = {
+            { "<leader>a", group = "ai" },
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
+    },
+  },
+
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    opts = {
+      input = { enabled = false },
+      select = { enabled = false },
+    },
+  },
+
+  {
+    "saghen/blink.compat",
+    lazy = true,
+    opts = {},
+    config = function()
+      -- monkeypatch cmp.ConfirmBehavior for Avante
+      require("cmp").ConfirmBehavior = {
+        Insert = "insert",
+        Replace = "replace",
+      }
+    end,
+  },
+
+  {
+    "saghen/blink.cmp",
+    lazy = true,
+    opts = {
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 100, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000, -- show at a higher priority than lsp
+            opts = {},
+          },
         },
-        ft = { "markdown", "Avante" },
       },
     },
   },
